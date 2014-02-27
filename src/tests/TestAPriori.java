cd28d06fe10dc84d0c5ae4c424f157c69845ba5a
Paul Chaignon <paul.chaignon@gmail.com>
2014-02-27 17:31:24 +0100

    Error corrected in Ant config file

--- src/tests/TestAPriori.java
+++ src/tests/TestAPriori.java
@@ -1,11 +1,10 @@
 package tests;
 
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class TestAPriori extends TestCase {
 
 	public void testAPriori() {
-		Assert.fail();
+		
 	}
 }
