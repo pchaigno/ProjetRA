d02f75b5a5553118bee0c99163fc54f7f261aedc
gwandalf <gwlemoul@insa-rennes.fr>
2014-03-12 09:36:55 +0100

    resources

--- src/ra/DataInterpreter.java
+++ src/ra/DataInterpreter.java
@@ -1,6 +1,11 @@
 package ra;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,13 +13,33 @@ public class DataInterpreter {
 	
 	/**
 	 * Computes the transaction list referring to some data
-	 * @param data File in wich the data are stored
+	 * @param data File in which the data are stored
 	 * @return initial transactions
+	 * @throws IOException fail if the file cannot be read
 	 */
-	public List<Transaction> interpret(File data) {
+	public List<Transaction> interpret(File data) throws IOException {
 		List<Transaction> res = new ArrayList<Transaction>();
-		
+		BufferedReader in = new BufferedReader(new FileReader(data));
+		String line = "";
+		while((line = in.readLine()) != null) {
+			
+		}
 		return res;
+	}
+	
+	//public List<Attributes> getAttributes()
+	
+	public static void main(String[] args) {
+		File abs = new File("");
+		String path = abs.getAbsolutePath();
+		System.out.println(path);
+		DataInterpreter di = new DataInterpreter();
+		File data  = new File(path + "/res/tickets_de_caisse.txt");
+		try {
+			di.interpret(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
