1f4de256981de21dd12598259453286b8c22f6d8
BrunoMatry <brunomatry@gmail.com>
2014-03-12 08:59:13 +0100

    Commentaires

--- src/ra/ClosAPriori.java
+++ src/ra/ClosAPriori.java
@@ -13,10 +13,12 @@ public class ClosAPriori extends APriori {
 		// Mise a jour de l'attribut itemsets
 		super.aPriori(minSupport);
 		
+		// Parcours des itemsets par rang
 		for(int i = 1 ; i < this.itemsets.size() ; i++) {
 			for(Itemset itemset: this.itemsets.get(i)) {
 				double support = itemset.calcSupport(this.transactions);
 				for(Itemset filsItemset: this.itemsets.get(i-1)){
+					// Si l'itemset de rang i-1 est inclus dans l'interset de rang i et son support est egal au spport de son pere
 					if(filsItemset.isIncludedIn(itemset) && filsItemset.calcSupport(this.transactions) == support)
 						this.itemsets.get(i-1).remove(filsItemset);
 				}
