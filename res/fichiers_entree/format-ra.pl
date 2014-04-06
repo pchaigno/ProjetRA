85223802ff53502601c8487d9ebece84973e6a78
Paul Chaignon <paul.chaignon@gmail.com>
2014-04-06 20:05:09 +0200

    Perl scripts cleaned up

--- res/fichiers_entree/format-ra.pl
+++ res/fichiers_entree/format-ra.pl
@@ -7,16 +7,18 @@ my $word;
 my $virg = 0;
 my $line;
 
-die "Usage: $0 dictionary in nb_articles out\n" unless ($#ARGV > -1);
+if($#ARGV != 3) {
+	die "Usage: $0 dictionary in nb_articles out\n";
+}
 my $file_in = $ARGV[0];
 my $data = $ARGV[1];
 my $nbArticles = $ARGV[2];
 my $file_out = $ARGV[3];
 
-open(OUT, "> $file_out") or die "unable to write $file_out $!\n";
+open(OUT, ">$file_out") or die "Unable to write $file_out:\n$!\n";
 
 # Iterates on all words from the dictionary to build the list:
-open(IN, $file_in) or die "Unable to read $file_in $!\n";
+open(IN, $file_in) or die "Unable to read $file_in:\n$!\n";
 while(<IN> =~ /([^\n]+)\n/) {
 	push @dictionary, $1;
 }
@@ -24,7 +26,7 @@ print OUT "\n";
 close IN;
 
 # Builds the ouput file:
-open(DATA, $data) or die "Unable to read $data $!\n";
+open(DATA, $data) or die "Unable to read $data:\n$!\n";
 while($line = <DATA>) {
 	my $empty_line = 1;
 	for(my $i=0; $i<=$#dictionary; $i++) {
