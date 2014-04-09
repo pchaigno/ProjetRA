3cbb427f6806febfbb4e40a91874e731290e65f0
Paul Chaignon <paul.chaignon@gmail.com>
2014-04-09 23:11:56 +0200

    Unit test on real file added for APriori

--- src/tests/TestAPriori.java
+++ src/tests/TestAPriori.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 import ra.algo.APriori;
 import ra.algo.Itemset;
+import ra.data.DataInterpreter;
 import ra.data.Transaction;
 
 public class TestAPriori extends TestCase {
@@ -35,13 +38,6 @@ public class TestAPriori extends TestCase {
 		List<Transaction> transactions = getTransactions();
 		APriori apriori = new APriori(transactions);
 		List<List<Itemset>> itemsets = apriori.aPriori(0.5);
-		for(int i=0; i<itemsets.size(); i++) {
-			System.out.println(i+1+"-itemsets:");
-			for(Itemset itemset: itemsets.get(i)) {
-				System.out.println(itemset);
-			}
-			System.out.println();
-		}
 		Assert.assertEquals(3, itemsets.size());
 		Assert.assertEquals(4, itemsets.get(1).size());
 		Assert.assertEquals(2, itemsets.get(1).get(0).size());
@@ -50,5 +46,22 @@ public class TestAPriori extends TestCase {
 		Assert.assertEquals(2, itemsets.get(1).get(3).size());
 		Assert.assertEquals(1, itemsets.get(2).size());
 		Assert.assertEquals(3, itemsets.get(2).get(0).size());
+	}
+	
+	/**
+	 * Tests the APriori algorithm on a real file.
+	 * @throws IOException 
+	 * @throws IllegalArgumentException 
+	 */
+	public void testAPrioriRealFile() throws IllegalArgumentException, IOException {
+		File data = new File("res/fichiers_entree/5027_articles.txt");
+		List<Transaction> transactions = DataInterpreter.interpret(data);
+		APriori ap = new APriori(transactions);
+		List<List<Itemset>> itemsets = ap.aPriori(0.0413650465);
+		int totalSize = 0;
+		for(int i=0; i<itemsets.size(); i++) {
+			totalSize += itemsets.get(i).size();
+		}
+		Assert.assertEquals(5378, totalSize);
 	}
 }
