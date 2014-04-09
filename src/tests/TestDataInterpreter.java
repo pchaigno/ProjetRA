a47b25b7b7e73cd835304dc24785c1bf485bdd1d
gwandalf <gwlemoul@insa-rennes.fr>
2014-04-10 00:22:32 +0200

    conflit representation des donnees

--- src/tests/TestDataInterpreter.java
+++ src/tests/TestDataInterpreter.java
@@ -24,4 +24,49 @@ public class TestDataInterpreter extends TestCase {
 			Assert.fail(e.getMessage());
 		}
 	}
+	
+	@Test
+	public static void testTickets() {
+		File tickets = new File("res/fichiers_entree/tickets_de_caisse.txt");
+		try {
+			List<Transaction> transactions = DataInterpreter.interpret(tickets);
+			Assert.assertEquals(1000, transactions.size());
+		} catch (IllegalArgumentException | IOException e) {
+			Assert.fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public static void testInitValueOf() {
+		String attributes = "Je Tu Il";
+		String[] splitted = DataInterpreter.initValueOf(attributes);
+		
+		Assert.assertEquals(3, splitted.length);
+		Assert.assertEquals("Je", splitted[0]);
+		Assert.assertEquals("Tu", splitted[1]);
+		Assert.assertEquals("Il", splitted[2]);
+	}
+	
+	@Test
+	public static void testValueOf() {
+		String attributes = "Je Tu Il";
+		
+		DataInterpreter.initValueOf(attributes);
+		
+		DataInterpreter.setValueOf("Je", "Lundi");
+		DataInterpreter.setValueOf("Tu", "Mardi");
+		DataInterpreter.setValueOf("Il", "Mercredi");
+		
+		DataInterpreter.setValueOf("Je", "Lundi");
+		DataInterpreter.setValueOf("Tu", "Fevrier");
+		DataInterpreter.setValueOf("Il", "Mars");
+		
+		Assert.assertEquals(1, DataInterpreter.getValueOf("Je", "Lundi"));
+		Assert.assertEquals(2, DataInterpreter.getValueOf("Tu", "Mardi"));
+		Assert.assertEquals(3, DataInterpreter.getValueOf("Il", "Mercredi"));
+		Assert.assertEquals(4, DataInterpreter.getValueOf("Tu", "Fevrier"));
+		Assert.assertEquals(5, DataInterpreter.getValueOf("Il", "Mars"));
+		
+	}
+	
 }
