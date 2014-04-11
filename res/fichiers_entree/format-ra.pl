68d035e331cca052adb091556dcaccf90a509124
Paul Chaignon <paul.chaignon@gmail.com>
2014-04-11 20:52:36 +0200

    Perl script cleaned up

--- res/fichiers_entree/format-ra.pl
+++ res/fichiers_entree/format-ra.pl
@@ -1,40 +1,37 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+use strict;
+
 use constant false => 0;
 use constant true => 1;
 
 my @dictionary;
-my $word;
-my $virg = 0;
-my $line;
 
 if($#ARGV != 2) {
 	die "Usage: $0 dictionary in out\n";
 }
-my $file_in = $ARGV[0];
-my $data = $ARGV[1];
-my $file_out = $ARGV[2];
-
-open(OUT, ">$file_out") or die "Unable to write $file_out:\n$!\n";
+my $dictionary_file = $ARGV[0];
+my $data_file = $ARGV[1];
+my $output_file = $ARGV[2];
 
 # Iterates on all words from the dictionary to build the list:
-open(IN, $file_in) or die "Unable to read $file_in:\n$!\n";
-while(<IN> =~ /([^\n]+)\n/) {
-	push @dictionary, $1;
+open(DICTIONARY, "< $dictionary_file") or die "Unable to read $dictionary_file:\n$!\n";
+while(my $word = <DICTIONARY>) {
+	chomp($word);
+	push(@dictionary, $word);
 }
-print OUT "\n";
-close IN;
+close(DICTIONARY);
 
 # Builds the ouput file:
-open(DATA, $data) or die "Unable to read $data:\n$!\n";
-while($line = <DATA>) {
-	my $empty_line = 1;
+open(OUT, "> $output_file") or die "Unable to write $output_file:\n$!\n";
+open(ARTICLES, "< $data_file") or die "Unable to read $data_file:\n$!\n";
+while(my $article = <ARTICLES>) {
+	my $empty_line = true;
 	for(my $i=0; $i<=$#dictionary; $i++) {
-		$word = $dictionary[$i];
-		print "$word\n";
+		my $word = $dictionary[$i];
 		# Print word's number if the word is in the article:
-		if($line =~ /\b$word\b/) {
+		if($article =~ /\b$word\b/) {
 			print OUT ($i+1).' ';
-			$empty_line = 0;
+			$empty_line = false;
 		}
 	}
 	# Doesn't print empty lines:
@@ -42,6 +39,6 @@ while($line = <DATA>) {
 		print OUT "\n";
 	}
 }
-close DATA;
 
-close OUT;
+close(ARTICLES);
+close(OUT);
