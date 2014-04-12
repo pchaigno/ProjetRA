5f3f4d8b2f52dd8ca702daf4b4ad669b8183b7ed
Ulysse Goarant <ulysses.goarant@gmail.com>
2014-04-12 10:57:33 +0200

    Removed unncessary Integer instanciations, remplaced by cast to avoid method ambiguity

--- src/tests/TestRule.java
+++ src/tests/TestRule.java
@@ -23,21 +23,21 @@ public class TestRule extends TestCase {
 		List<Rule> derivedRules = r.deriveRules();
 
 		// 2, 3 -> 4, 1
-		Assert.assertEquals(new Integer(2), derivedRules.get(0).getAntecedent().get(0));
-		Assert.assertEquals(new Integer(3), derivedRules.get(0).getAntecedent().get(1));
-		Assert.assertEquals(new Integer(4), derivedRules.get(0).getConsequent().get(0));
-		Assert.assertEquals(new Integer(1), derivedRules.get(0).getConsequent().get(1));
+		Assert.assertEquals((Integer) 2, derivedRules.get(0).getAntecedent().get(0));
+		Assert.assertEquals((Integer) 3, derivedRules.get(0).getAntecedent().get(1));
+		Assert.assertEquals((Integer) 4, derivedRules.get(0).getConsequent().get(0));
+		Assert.assertEquals((Integer) 1, derivedRules.get(0).getConsequent().get(1));
 		
 		// 1, 3 -> 4, 2
-		Assert.assertEquals(new Integer(1), derivedRules.get(1).getAntecedent().get(0));
-		Assert.assertEquals(new Integer(3), derivedRules.get(1).getAntecedent().get(1));
-		Assert.assertEquals(new Integer(4), derivedRules.get(1).getConsequent().get(0));
-		Assert.assertEquals(new Integer(2), derivedRules.get(1).getConsequent().get(1));
+		Assert.assertEquals((Integer) 1, derivedRules.get(1).getAntecedent().get(0));
+		Assert.assertEquals((Integer) 3, derivedRules.get(1).getAntecedent().get(1));
+		Assert.assertEquals((Integer) 4, derivedRules.get(1).getConsequent().get(0));
+		Assert.assertEquals((Integer) 2, derivedRules.get(1).getConsequent().get(1));
 		
 		// 1,2 -> 4, 3
-		Assert.assertEquals(new Integer(1), derivedRules.get(2).getAntecedent().get(0));
-		Assert.assertEquals(new Integer(2), derivedRules.get(2).getAntecedent().get(1));
-		Assert.assertEquals(new Integer(4), derivedRules.get(2).getConsequent().get(0));
-		Assert.assertEquals(new Integer(3), derivedRules.get(2).getConsequent().get(1));
+		Assert.assertEquals((Integer) 1, derivedRules.get(2).getAntecedent().get(0));
+		Assert.assertEquals((Integer) 2, derivedRules.get(2).getAntecedent().get(1));
+		Assert.assertEquals((Integer) 4, derivedRules.get(2).getConsequent().get(0));
+		Assert.assertEquals((Integer) 3, derivedRules.get(2).getConsequent().get(1));
 	}
 }
